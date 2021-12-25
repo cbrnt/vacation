5b7a4e1c95311b4a559804e6490d683253253c9f
cbrnt <3cbrnt@gmail.com>
2021-12-26 00:39:11 +0300

    запись в базу

--- vacation.py
+++ vacation.py
@@ -1,17 +1,13 @@
 import os
 from datetime import datetime
-# import pandas as pd
-
 import requests
-from base64 import b64encode
-import base64
 from json import loads
+import sqlite3
+
+DB_FILE = 'vacation.db'
 
 LIVE_URL = os.environ['LIVE_URL']
 API_KEY = os.environ['API_KEY']
-
-
-
 
 this_year = datetime.today().strftime('%Y')
 
@@ -36,13 +32,11 @@ iter_month = '10'
 #
 # parser = MyHTMLParser()
 
-
 live3 = requests.Session()
-
 live3.params = {'api_key': API_KEY}
 api_path = '/api/users/vacation'
+# получаем список всех отпусков за все времена
 get_vacations = live3.get(LIVE_URL + api_path)
-
 vacations_this_year = dict()
 
 if get_vacations.status_code == 200:
@@ -53,20 +47,28 @@ if get_vacations.status_code == 200:
             if vacation['reason'] == 'vacation':
                 if not vacations_this_year.get(vacation['user_id']):
                     vacations_this_year[vacation['user_id']] = []
-
                 vacations_this_year[vacation['user_id']].append(vacation_date)
 
 
+# подключаемся к sqlite3
+dbConnect = sqlite3.connect(DB_FILE)
+setCursor = dbConnect.cursor()
+# создаем таблицу, если не существует
+setCursor.execute('SELECT name FROM sqlite_master WHERE type="table" AND name="vacations"')
+if not setCursor.fetchall():
+    setCursor.execute('''CREATE TABLE vacations
+                        (user_id text, name text, vacation_dates text)''')
 
+setCursor.execute('SELECT * FROM vacations ORDER BY vacation_dates')
+# проверяем пуста ли база
+if setCursor.fetchall():
+    condition = True
+    print('True. Have something')
+else:
+    condition = False
+    print('False. Have nothing')
 
-
-
-
-
-
-
-
-
-
-
+setCursor.execute("INSERT INTO vacations VALUES ('%s', '%s', '%s')" % ('1302', 'Edward', '2021-10-03'))
+dbConnect.commit()
+dbConnect.close()
 pass
