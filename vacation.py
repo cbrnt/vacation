6f50242992eeb99a407bcfe29bb6daa12d3792ae
Вигерин Эдуард <vigerin@tochkak.ru>
2022-01-12 19:20:13 +0300

    разделил на функции

--- vacation.py
+++ vacation.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import requests
 from json import loads
 import sqlite3
+import pandas as pd
 
 DB_FILE = 'vacation.db'
 
@@ -11,7 +12,7 @@ API_KEY = os.environ['API_KEY']
 
 this_year = datetime.today().strftime('%Y')
 
-iter_month = '10'
+# iter_month = '10'
 # get_timeline = 'timeline?department_id=&team_id=&date=' + '%s' % year + '%2F' + '%s&q=' % iter_month
 # print
 
@@ -32,43 +33,48 @@ iter_month = '10'
 #
 # parser = MyHTMLParser()
 
-live3 = requests.Session()
-live3.params = {'api_key': API_KEY}
-api_path = '/api/users/vacation'
-# получаем список всех отпусков за все времена
-get_vacations = live3.get(LIVE_URL + api_path)
-vacations_this_year = dict()
 
-if get_vacations.status_code == 200:
-    vacations_all = loads(get_vacations.content)
-    for vacation in vacations_all:
-        vacation_date = datetime.strptime(vacation['date'], '%Y-%m-%d')
-        if vacation_date.year == int(this_year):
-            if vacation['reason'] == 'vacation':
-                if not vacations_this_year.get(vacation['user_id']):
-                    vacations_this_year[vacation['user_id']] = []
-                vacations_this_year[vacation['user_id']].append(vacation_date)
+def vacations() -> dict:
+    """берет из API отпуска за текущий год"""
+    live3 = requests.Session()
+    live3.params = {'api_key': API_KEY}
+    api_path = '/api/users/vacation'
+    get_vacations = live3.get(LIVE_URL + api_path)
+    vacations_this_year = dict()
+    if get_vacations.status_code == 200:
+        vacations_all = loads(get_vacations.content)
+        for vacation in vacations_all:
+            vacation_date = datetime.strptime(vacation['date'], '%Y-%m-%d')
+            if vacation_date.year == int(this_year):
+                if vacation['reason'] == 'vacation':
+                    if not vacations_this_year.get(vacation['user_id']):
+                        vacations_this_year[vacation['user_id']] = []
+                    vacations_this_year[vacation['user_id']].append(vacation_date)
+    return vacations_this_year
 
 
-# подключаемся к sqlite3
-dbConnect = sqlite3.connect(DB_FILE)
-setCursor = dbConnect.cursor()
-# создаем таблицу, если не существует
-setCursor.execute('SELECT name FROM sqlite_master WHERE type="table" AND name="vacations"')
-if not setCursor.fetchall():
-    setCursor.execute('''CREATE TABLE vacations
-                        (user_id text, name text, vacation_dates text)''')
+class Db:
+    def __init__(self):
+        self.db_connect = sqlite3.connect(DB_FILE)
+        self.set_cursor = self.db_connect.cursor()
+        # создаем таблицу, если не существует
+        self.set_cursor.execute('SELECT name FROM sqlite_master WHERE type="table" AND name="vacations"')
+        if not self.set_cursor.fetchall():
+            self.set_cursor.execute('''CREATE TABLE vacations
+                                (user_id text, name text, vacation_dates text)''')
 
-setCursor.execute('SELECT * FROM vacations ORDER BY vacation_dates')
-# проверяем пуста ли база
-if setCursor.fetchall():
-    condition = True
-    print('True. Have something')
-else:
-    condition = False
-    print('False. Have nothing')
+    def read(self, user_id='all'):
+        # todo добавь выборку по полю id пользователя
+        self.setCursor.execute('SELECT * FROM vacations ORDER BY vacation_dates')
+        return self.setCursor.fetchall()
 
-setCursor.execute("INSERT INTO vacations VALUES ('%s', '%s', '%s')" % ('1302', 'Edward', '2021-10-03'))
-dbConnect.commit()
-dbConnect.close()
+
+    def write(self):
+        self.setCursor.execute("INSERT INTO vacations VALUES ('%s', '%s', '%s')" % ('1302', 'Edward', '2021-10-03'))
+        self.dbConnect.commit()
+        self.dbConnect.close()
+
+
+get_data = vacations()
+search_date = (datetime.today() + pd.DateOffset(months=1)).strftime("%d-%m-%Y")
 pass
